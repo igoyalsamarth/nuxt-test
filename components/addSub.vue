df6c8f6a0e3026a42e783c13ed793a81628c3ece
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-07-22 14:30:08 +0530

    initial commit

---
+++ components/addSub.vue
@@ -0,0 +1,15 @@
+<script setup>
+const number = useState("number", () => 0)
+const show = useState('show', () => false)
+</script>
+
+<template>
+    <div>
+        <p>number: {{ number }}</p>
+        <p>show : {{ show }}</p>
+        <button @click="number++">Add</button>
+        <button @click="number--">Minus</button>
+        <button @click="show = !show">Show</button>
+        <HideShow :show="show" />
+    </div>
+</template>
---
+++ components/hideShow.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+    show: {
+        type: Boolean,
+    },
+});
+const show = props;
+</script>
+
+<template>
+    <p v-if="show.show">Show</p>
+    <p v-else>Hide</p>
+</template>
